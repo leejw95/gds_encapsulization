dd30109af6f095627e4f17f55e2eafb1f0553329
junung0110 <junung@postech.ac.kr>
2022-07-04 06:20:17 +0000

    from docker

---
+++ auto_encrypted_test/Generator.py
@@ -0,0 +1,9 @@
+import d228727cc591c5a1c73031137d54a2e982c864f4b77ab94ab6834f8ee6788269
+
+libname = ['Tie_Cell (1)', 'NMOSWithDummy (2)', 'PMOSWithDummy (3)', 'SupplyRails (4)']
+
+print ("Select Generator list {}: ".format(libname))
+
+arg = d228727cc591c5a1c73031137d54a2e982c864f4b77ab94ab6834f8ee6788269
+
+exec(open(arg._1a325e9af1de76dc492589ba2d0c8ce69c8bb4a58a16a4a17608c3d58f462ad2[int(input())] + '.py').read())
