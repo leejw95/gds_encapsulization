af09c86c05f2a0239011e54952a3567103c050a7
junung0110 <junung@postech.ac.kr>
2022-07-04 16:19:10 +0900

    test.....

--- auto_encrypted_test/Generator.py
+++ auto_encrypted_test/Generator.py
@@ -1,9 +1,15 @@
 import d228727cc591c5a1c73031137d54a2e982c864f4b77ab94ab6834f8ee6788269
+import _1bebca3b5d3a8f929526cf732e8ffd879f12fa84d0597b88b0905649e668578f
 
-libname = ['Tie_Cell (1)', 'NMOSWithDummy (2)', 'PMOSWithDummy (3)', 'SupplyRails (4)']
+libname = ['Tie_Cell', 'NMOSWithDummy', 'PMOSWithDummy', 'SupplyRails']
 
 print ("Select Generator list {}: ".format(libname))
 
-arg = d228727cc591c5a1c73031137d54a2e982c864f4b77ab94ab6834f8ee6788269
+a = int(input())
 
-exec(open(arg._1a325e9af1de76dc492589ba2d0c8ce69c8bb4a58a16a4a17608c3d58f462ad2[int(input())] + '.py').read())
+d228727cc591c5a1c73031137d54a2e982c864f4b77ab94ab6834f8ee6788268 = d228727cc591c5a1c73031137d54a2e982c864f4b77ab94ab6834f8ee6788269
+
+c = d228727cc591c5a1c73031137d54a2e982c864f4b77ab94ab6834f8ee6788268.e365cd9c617713a54c6455f9d8998ba16e81e5078585f33a2ddca8f03a3e8593(libname[a])
+
+print (c)
+exec(open(c + '.py').read())
