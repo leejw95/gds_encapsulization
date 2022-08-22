7cc8c2f0e6594a21dd5261fc7982ab750aee2122
junung0110 <junung@postech.ac.kr>
2022-08-22 23:58:40 +0900

    constance string 만 수정

--- test.py
+++ test.py
@@ -1,2 +1,2 @@
-for j in range (1,7) :
-					name12 = 'mux_module_%d'%j
+self._DesignParameter['INV']['_DesignObj']._CalculateDesignParameter_v3(**dict(_Finger=INV_finger, _ChannelWidth=INV_nmos_width, _ChannelLength=gate_length, _NPRatio=2, _VDD2VSSHeight=Cell_height, _VDD2PMOSHeight=VDD2PMOS, _VSS2NMOSHeight=NMOS_y, _YCoordOfInput=None, _Dummy=True, _XVT=XVT, _GateSpacing=gate_spacing, _SDWidth=66, _NumViaPMOSMet12Met2CoY=None, _NumViaNMOSMet12Met2CoY=None, _SupplyRailType=2))
+self._DesignParameter['INV']['_XYCoordinates'] = [[0.0, 0.0]]
